0a8e6f04bc00ef9749c45a09b04b889ba27e4f57
Daniel Holmes <DanielJholmes@hotmail.co.uk>
2019-12-01 21:51:26 +0000

    [ada] Day_01-Star_02

--- Day_01/src/rocket_maths.adb
+++ Day_01/src/rocket_maths.adb
@@ -3,22 +3,40 @@ use Array_Stuff;
 
 package body Rocket_Maths is
 
-   
    function Calculate_Fuel_Cost(mass: Integer) return Integer is
    begin
       return mass/3 - 2;
-   end Calculate_Fuel_Cost;
-
+   end Calculate_Fuel_Cost;  
    
-   function Get_Required_Fuel(masses: Int_Arr) return Integer is
+   
+   function Calculate_Recursive_Fuel_Cost(mass: Integer) return Integer is
+      fuel: Integer:= Calculate_Fuel_Cost(mass);
+   begin
+      if fuel < 0 then
+         return 0;
+      else
+         return fuel + Calculate_Recursive_Fuel_Cost(fuel);
+      end if;
+   end Calculate_Recursive_Fuel_Cost;
+   
+   
+   function Get_Required_Fuel(masses: Int_Arr; recursive:Boolean) return Integer is
       function Get_Each_Fuel_Cost is new Apply_To_Elements(T => Integer,
                                                            T_Arr => Int_Arr,
                                                            Transform => Calculate_Fuel_Cost);
+         
+      function Get_Each_Fuel_Cost_Recursive is new Apply_To_Elements(T => Integer,
+                                                                     T_Arr => Int_Arr,
+                                                                     Transform => Calculate_Recursive_Fuel_Cost);
       function Sum_Ints is new Sum_Elements(T => Integer,
                                             T_Arr => Int_Arr);
       fuel_needed :Int_Arr(masses'Range);
    begin
-      fuel_needed:=Get_Each_Fuel_Cost(masses);
+      if recursive then
+         fuel_needed:=Get_Each_Fuel_Cost_Recursive(masses);
+      else
+         fuel_needed:=Get_Each_Fuel_Cost(masses);
+      end if;
       return Sum_Ints(fuel_needed, 0);
    end Get_Required_Fuel;
 
--- Day_01/src/rocket_maths.ads
+++ Day_01/src/rocket_maths.ads
@@ -4,6 +4,7 @@ use Array_Stuff;
 package Rocket_Maths is
    
    function Calculate_Fuel_Cost(mass: Integer) return Integer;
-   function Get_Required_Fuel(masses: Int_Arr) return Integer;
+   function Calculate_Recursive_Fuel_Cost(mass: Integer) return Integer;
+   function Get_Required_Fuel(masses: Int_Arr; recursive: Boolean) return Integer;
 
 end Rocket_Maths;
