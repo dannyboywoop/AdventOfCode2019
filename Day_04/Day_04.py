d855ee31d132a0eb37d8f382f97f5d08969897e9
Daniel Holmes <DanielJholmes@hotmail.co.uk>
2019-12-04 23:59:58 +0000

    [python] Day_04-Star_02

--- Day_04/Day_04.py
+++ Day_04/Day_04.py
@@ -20,18 +20,49 @@ def repeated_digit(value):
     return False
 
 
-def meets_criteria(value):
+def double_digit(value):
+    previous_digit = ""
+    repetition_count = 0
+    for digit in str(value):
+        if digit == previous_digit:
+            repetition_count += 1
+        else:
+            if repetition_count == 2:
+                return True
+            repetition_count = 1
+        previous_digit = digit
+    if repetition_count == 2:
+        return True
+    return False
+
+
+def meets_star_one_criteria(value):
     return always_increasing(value) and repeated_digit(value)
 
 
-def enumerate_passwords():
+def meets_star_two_criteria(value):
+    return always_increasing(value) and double_digit(value)
+
+
+def star_one_passwords():
     valid_passwords = []
     for potential_password in range(PUZZLE_INPUT[0], PUZZLE_INPUT[1]):
-        if meets_criteria(potential_password):
+        if meets_star_one_criteria(potential_password):
+            valid_passwords.append(potential_password)
+    return valid_passwords
+
+
+def star_two_passwords():
+    valid_passwords = []
+    for potential_password in range(PUZZLE_INPUT[0], PUZZLE_INPUT[1]):
+        if meets_star_two_criteria(potential_password):
             valid_passwords.append(potential_password)
     return valid_passwords
 
 
 if __name__ == "__main__":
-    star_one = len(enumerate_passwords())
+    star_one = len(star_one_passwords())
     print("Star one: {}".format(star_one))
+
+    star_two = len(star_two_passwords())
+    print("Star two: {}".format(star_two))
