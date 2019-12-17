5620d1c511df3445611f07f598b9376126a132d4
Daniel Holmes <DanielJholmes@hotmail.co.uk>
2019-12-17 22:34:53 +0000

    [python] Day_07-Refactor

--- Day_07/python/Day_07.py
+++ Day_07/python/Day_07.py
@@ -1,5 +1,4 @@
 from itertools import permutations
-from copy import deepcopy
 import Intcode
 PHASE_SETTINGS = range(5)
 FEEDBACK_PHASE_SETTINGS = range(5, 10)
@@ -7,12 +6,12 @@ FEEDBACK_PHASE_SETTINGS = range(5, 10)
 
 class Amplifier():
     def __init__(self, program, phase):
-        self.program = program
+        self.computer = Intcode.Intcode_Computer(program)
         self.phase = phase
         self.phase_set = False
+        self.output = []
 
     def amplify(self, initial_val):
-        output = []
 
         def mock_input(input_request):
             if not self.phase_set:
@@ -21,15 +20,15 @@ class Amplifier():
             return initial_val
 
         Intcode.input = mock_input
-        Intcode.print = lambda s: output.append(s)
-        Intcode.process(program)
-        return output[0]
+        Intcode.print = lambda s: self.output.append(s)
+        self.computer.process_all()
+        return self.output[-1]
 
 
 def run_amplification_circuit(program, phase_data, initial_val=0):
     output = initial_val
     for phase in phase_data:
-        amplifier = Amplifier(deepcopy(program), phase)
+        amplifier = Amplifier(program, phase)
         output = amplifier.amplify(output)
     return output
 
